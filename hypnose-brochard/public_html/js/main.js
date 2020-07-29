b60a259023df1e06e10ac73d23958fd021de886b
Brochard <all.brochard@gmail.com>
2020-07-29 18:01:23 +0200

    <EVO> Header footer final + filtre blanc du background

--- hypnose-brochard/public_html/js/main.js
+++ hypnose-brochard/public_html/js/main.js
@@ -1,9 +1,20 @@
 
 $(document).ready(function(){
-    $(".dropdown, .btn-group").hover(function(){
-        var dropdownMenu = $(this).children(".dropdown-menu");
-        if(dropdownMenu.is(":visible")){
-            dropdownMenu.parent().toggleClass("open");
+    if((window.matchMedia("(min-width: 990px)")).matches){
+        $(".dropdown, .btn-group").hover(function(){
+            var dropdownMenu = $(this).children(".dropdown-menu");
+            if(dropdownMenu.is(":visible")){
+                dropdownMenu.parent().toggleClass("open");
+            }
+        });
+    }
+    $("#button-collapse").click(function(){
+        var element = document.getElementById('navbarSupportedContent');
+        if( element.classList.contains('show')){
+            element.classList.remove('show');
+        }
+        else{
+            element.classList.add('show');
         }
     });
 });   
